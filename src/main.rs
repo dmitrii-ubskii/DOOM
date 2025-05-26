6d2b7c7be8d7887977b89ce6960510e5033aacf6
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2025-05-26 20:24:58 +0100

    rust main

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::{env, ffi::{c_char, CString}};
+
+unsafe extern "C" {
+    static mut myargc: i32;
+    static mut myargv: *const *const c_char;
+    fn D_DoomMain();
+}
+
+fn main() {
+	let args: Vec<_> = env::args().map(|arg| CString::new(arg).unwrap()).collect();
+	let argv: Vec<_> = args.iter().map(|cstring| cstring.as_ptr()).collect();
+    unsafe {
+        myargc = args.len() as i32;
+		myargv = argv.as_ptr();
+        D_DoomMain();
+    }
+}
