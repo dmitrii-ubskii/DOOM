da1f6c1cd9966596505e78effb098a36855107c8
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2025-10-04 22:55:51 +0100

    fix div by zero

--- src/p_pspr.rs
+++ src/p_pspr.rs
@@ -598,7 +598,7 @@ pub(crate) fn A_BFGSpray(mo: &mut mobj_t) {
 	unsafe {
 		// offset angles from its attack angle
 		for i in 0..40 {
-			let an = mo.angle - (ANG90 / Wrapping(2) - ANG90 / Wrapping(40 * i));
+			let an = mo.angle - (ANG90 / Wrapping(2) - ANG90 / Wrapping(40) * Wrapping(i));
 
 			// mo.target is the originator (player)
 			//  of the missile
