951153954c46a9d3fc67a08e0c6a12c2f8f0bac2
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2026-09-12 19:02:36 +0100

    we no longer build C sources or link to libm

--- build.rs
+++ build.rs
@@ -1,6 +1,4 @@
 fn main() {
-	println!("cargo::rerun-if-changed=linuxdoom-1.10");
 	println!("cargo::rustc-link-lib=Xext");
 	println!("cargo::rustc-link-lib=X11");
-	println!("cargo::rustc-link-lib=m");
 }
